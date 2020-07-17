7a634a68b2698a8d9c18309d16f5532e59932253
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-17 13:48:06 -0600

    Input and print order
    Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.

---
+++ PrintOrder.py
@@ -0,0 +1,6 @@
+# this exercise asks for a user's first and last name as inputs and wants me to print them in reverse order
+
+first = input("Enter first name: ")
+last = input("Enter last name: ")
+
+print(last, first)
