4bd58c39b725d440408b4efeb8afb3a9118c9424
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-17 00:25:49 -0600

    Add files via upload
    Write a Python program to get the Python version you are using.
    
    A string containing the version number of the Python interpreter plus additional information on the build number and compiler used. This string is displayed when the interactive interpreter is started.

---
+++ PythonVersion.py
@@ -0,0 +1,9 @@
+# An exercise to show the Python version I'm currently using
+# The sys module has to be imported because it isn't part of the Python Standard Library
+
+import sys
+
+print("Python version")
+print(sys.version)
+print("Version info")
+print(sys.version_info)
