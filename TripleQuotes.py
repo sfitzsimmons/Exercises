53847b0f59b404a9e48e176a68a87ca74b9b19f0
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-20 22:04:15 -0600

    Using triple quotes
    Write a Python program to print the following here document.
    Sample string :
    a string that you "don't" have to escape
    This
    is a ....... multi-line
    heredoc string --------> example

---
+++ TripleQuotes.py
@@ -0,0 +1,14 @@
+# This exercise just wants me to print a document using formatted printing
+
+# all I have to use is triple quotes
+# It wanted to trip me up with the "don't" because it uses both " and '.
+# This could still be done using escapes \
+
+print("""
+a string that you "don't" have to escape
+This
+is a......multi-line
+heredoc string ----------> example
+""")
+
+
