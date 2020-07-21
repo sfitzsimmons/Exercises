248db163cf14106cdc52396581a66c4914dd949b
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-20 21:18:05 -0600

    Tuple extraction and formatted string
    Write a Python program to display the examination schedule. (extract the date from exam_st_date). 
    exam_st_date = (11, 12, 2014)
    Sample Output : The examination will start from : 11 / 12 / 2014

---
+++ ScheduleDisplay.py
@@ -0,0 +1,5 @@
+# This exercise asks me to extract a date stored in a tuple and add it to a string that I print off
+
+exam_st_date = (11, 12, 2014)
+
+print(f"The examination will start from: {exam_st_date[0]} / {exam_st_date[1]} / {exam_st_date[2]}")
