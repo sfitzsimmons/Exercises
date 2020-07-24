dff61575f8b90241446f4457b66cb91bb90d6ec7
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-23 19:31:50 -0600

    Using abs()
    Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.

---
+++ DistanceFrom17.py
@@ -0,0 +1,8 @@
+# this exercise wants me to write a program that determines the difference between a given number and 17.
+
+given_number = int(input("Enter a number: "))
+
+difference = abs(given_number - 17)
+
+print(f"The difference between your number ({given_number}) and 17 is: {difference}.")
+
