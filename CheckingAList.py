851380523d9c798b961846401fd78c1639184dfc
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-30 13:15:12 -0600

    Checking a list for a value
     Write a Python program to check whether a specified value is contained in a group of values. 
    Test Data :
    3 -> [1, 5, 8, 3] : True
    -1 -> [1, 5, 8, 3] : False

---
+++ CheckingAList.py
@@ -0,0 +1,8 @@
+# this exercise asks for me to check whether a specified value is contained in a group of values.
+
+test_data = [1, 4, 8, 3]
+
+test_number = int(input("Enter number to check for: "))
+
+print("Is number in test data? ", test_number in test_data)
+
