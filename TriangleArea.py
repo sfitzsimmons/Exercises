08b38195883d0216de9ebca674e668a605a7d43e
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-07-30 22:13:27 -0600

    Calculating the area of a triangle
    Write a Python program that will accept the base and height of a triangle and compute the area.

---
+++ TriangleArea.py
@@ -0,0 +1,9 @@
+# this exercise wants me to take the base and the height of a triangle and calculate the area.
+
+height = float(input("Height: "))
+width = float(input("Width: "))
+
+area = height * width / 2
+
+print(f"Triangle area: {area}")
+
