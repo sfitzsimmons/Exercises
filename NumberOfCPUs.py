2d333b7f049b6e3685b27fb7e7ae58bac6c15f55
Sean Fitzsimmons <62264286+sfitzsimmons@users.noreply.github.com>
2020-08-05 13:16:00 -0600

    Finding number of CPUs being used
    Write a Python program to find out the number of CPUs using.

---
+++ NumberOfCPUs.py
@@ -0,0 +1,6 @@
+# this exercise asks for me to find out the number of CPUs being used.
+
+import multiprocessing
+
+print("The current number of CPUs being used is: ", multiprocessing.cpu_count())
+
